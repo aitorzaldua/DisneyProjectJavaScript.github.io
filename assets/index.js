2869136589706df17a4a03e562ee252779cf2b85
Aitor Zaldua <aitor.zaldua@gmail.com>
2020-08-10 14:57:19 +0200

    last commit

---
+++ assets/index.js
@@ -0,0 +1,40 @@
+
+var video = document.getElementById("video");
+  video.addEventListener("click", function(event) {
+        if (video.paused == true) {
+             video.play();
+        }
+        else{
+             video.pause();
+        }
+  });
+
+  var video2 = document.getElementById("video2");
+  video2.addEventListener("click", function(event) {
+        if (video2.paused == true) {
+             video2.play();
+        }
+        else{
+             video2.pause();
+        }
+  });
+
+  var video3 = document.getElementById("video3");
+  video3.addEventListener("click", function(event) {
+        if (video3.paused == true) {
+             video3.play();
+        }
+        else{
+             video3.pause();
+        }
+  });
+
+  var video4 = document.getElementById("video4");
+  video4.addEventListener("click", function(event) {
+        if (video4.paused == true) {
+             video4.play();
+        }
+        else{
+             video4.pause();
+        }
+  });
